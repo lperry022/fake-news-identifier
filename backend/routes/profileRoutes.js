d41a30ef8907eea40d06d7158fc1251a7efb12db
Liana Perry <62174756+lperry022@users.noreply.github.com>
2025-09-14 14:49:31 +1000

    Creating sign up pages

--- backend/routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { requireAuth } from "../middleware/requireAuth.js";
-import { getProfile, updateProfile } from "../controllers/profileController.js";
-
-export const profileRouter = Router();
-
-profileRouter.get("/", requireAuth, getProfile);
-profileRouter.put("/", requireAuth, updateProfile);
